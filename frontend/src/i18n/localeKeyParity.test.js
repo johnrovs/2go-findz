6a6db22b97109f8b8ad68d44a54fbdf0bbcea080
johnrovs <roverorommel12@gmail.com>
2026-08-12 13:05:00 -0700

    feat(i18n): add guides namespace translations for all 5 launch locales

--- frontend/src/i18n/localeKeyParity.test.js
+++ frontend/src/i18n/localeKeyParity.test.js
@@ -1,9 +1,14 @@
 import { describe, expect, it } from 'vitest';
-import enUS from './locales/en-US/common.json';
-import esUS from './locales/es-US/common.json';
-import filPH from './locales/fil-PH/common.json';
-import zhHans from './locales/zh-Hans/common.json';
-import vi from './locales/vi/common.json';
+import commonEnUS from './locales/en-US/common.json';
+import commonEsUS from './locales/es-US/common.json';
+import commonFilPH from './locales/fil-PH/common.json';
+import commonZhHans from './locales/zh-Hans/common.json';
+import commonVi from './locales/vi/common.json';
+import guidesEnUS from './locales/en-US/guides.json';
+import guidesEsUS from './locales/es-US/guides.json';
+import guidesFilPH from './locales/fil-PH/guides.json';
+import guidesZhHans from './locales/zh-Hans/guides.json';
+import guidesVi from './locales/vi/guides.json';
 
 function flattenKeys(obj, prefix = '') {
   return Object.entries(obj).flatMap(([key, value]) => {
@@ -12,15 +17,26 @@ function flattenKeys(obj, prefix = '') {
   });
 }
 
-describe('common.json locale key parity', () => {
-  const englishKeys = flattenKeys(enUS).sort();
+function describeParity(namespace, enUS, translations) {
+  describe(`${namespace}.json locale key parity`, () => {
+    const englishKeys = flattenKeys(enUS).sort();
 
-  it.each([
-    ['es-US', esUS],
-    ['fil-PH', filPH],
-    ['zh-Hans', zhHans],
-    ['vi', vi],
-  ])('%s has exactly the same keys as en-US', (_locale, translations) => {
-    expect(flattenKeys(translations).sort()).toEqual(englishKeys);
+    it.each(translations)('%s has exactly the same keys as en-US', (_locale, resource) => {
+      expect(flattenKeys(resource).sort()).toEqual(englishKeys);
+    });
   });
-});
+}
+
+describeParity('common', commonEnUS, [
+  ['es-US', commonEsUS],
+  ['fil-PH', commonFilPH],
+  ['zh-Hans', commonZhHans],
+  ['vi', commonVi],
+]);
+
+describeParity('guides', guidesEnUS, [
+  ['es-US', guidesEsUS],
+  ['fil-PH', guidesFilPH],
+  ['zh-Hans', guidesZhHans],
+  ['vi', guidesVi],
+]);
