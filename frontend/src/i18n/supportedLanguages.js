cda9cb5f60a3cdbe18d9f478da8abea3571901cb
johnrovs <roverorommel12@gmail.com>
2026-08-12 11:57:04 -0700

    feat(i18n): add i18next dependencies and supported-languages config

---
+++ frontend/src/i18n/supportedLanguages.js
@@ -0,0 +1,7 @@
+export const SUPPORTED_LANGUAGES = [
+  { code: 'en-US', nativeName: 'English' },
+  { code: 'es-US', nativeName: 'Español' },
+  { code: 'fil-PH', nativeName: 'Filipino' },
+  { code: 'zh-Hans', nativeName: '简体中文' },
+  { code: 'vi', nativeName: 'Tiếng Việt' },
+];
